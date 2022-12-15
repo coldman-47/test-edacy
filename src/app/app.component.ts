966d15e1762eab7bebd6e296ed829ed33405efa3
Coldman <ocamara015@gmail.com>
2022-12-15 22:27:29 +0000

    1st commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +7,17 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'test';
+  @ViewChild('image') image: any;
+
+  getFile(input: any, e: any){
+    e.preventDefault();
+    console.log(e.target);
+    
+  }
+
+  test(e: any){
+    console.log(e);
+    
+    alert()
+  }
 }
