1933475aae505ffc392e14676e539fc5df3f60ed
Coldman <ocamara015@gmail.com>
2022-12-16 00:06:56 +0000

    form

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component, ViewChild } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -8,16 +9,26 @@ import { Component, ViewChild } from '@angular/core';
 export class AppComponent {
   title = 'test';
   @ViewChild('image') image: any;
+  programForm: any;
+
+  constructor(fb: FormBuilder){
+    this.programForm = fb.group({
+      title: [null, Validators.required],
+      lang: [null, Validators.required],
+      level: [null, Validators.required],
+      type: [null, Validators.required],
+      desc: [null, Validators.required],
+      objectives: [null, Validators.required],
+      duration: [null, Validators.required],
+      hours: [null, Validators.required],
+      prequisite: [null, Validators.required],
+      certification: [null, Validators.required],
+      start: [null, Validators.required]
+    })
+  }
 
   getFile(input: any, e: any){
     e.preventDefault();
     console.log(e.target);
-    
-  }
-
-  test(e: any){
-    console.log(e);
-    
-    alert()
   }
 }
